b225e499eef815bef3a32316571377df3c74f746
mohitul-islam <mohitul449@gmail.com>
2025-11-22 22:51:59 +0600

    update migrate files

--- migrations/000002_roles.up.sql
+++ migrations/000002_roles.up.sql
@@ -1,14 +1,24 @@
+-- Active: 1760178178339@@127.0.0.1@3306@chat_service
 CREATE TABLE roles (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL UNIQUE,
-  description VARCHAR(255) NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (id)
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    description VARCHAR(255) NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO roles (name, description)
-VALUES
-  ('admin', 'Administrator with full permissions'),
-  ('moderator', 'Moderator with limited permissions'),
-  ('user', 'Default system user role');
+INSERT INTO
+    roles (name, description)
+VALUES (
+        'admin',
+        'Administrator with full permissions'
+    ),
+    (
+        'moderator',
+        'Moderator with limited permissions'
+    ),
+    (
+        'user',
+        'Default system user role'
+    );
