9e7a4af2afd718086abed700bf1cf6c6b4271af4
mohitul-islam <mohitul449@gmail.com>
2025-11-28 13:09:55 +0600

    setups with bun for project dev and tsup setups

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,33 +2,17 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/server.ts"],
-
   format: ["esm"],
-
-  outDir: "dist",
-  target: "node22",
-
   dts: true,
-  splitting: false, // backend => no code splitting needed
+  splitting: false,
   sourcemap: true,
   clean: true,
+  target: "node22",
+  outDir: "dist",
 
-  // Node.js features (process, fs, buffer, globals)
   platform: "node",
 
-  // Enable this to include .env or other assets if needed
-  // assets: { baseDir: "src", include: ["config/**/*.yaml"] },
-
-  // Keep dynamic imports intact
-  shims: false,
-
-  // Don't minify — debugging backend is easier
-  minify: false,
-
-  // Ensure ESM imports resolve correctly (.js)
   esbuildOptions(options) {
-    options.outExtension = {
-      ".js": ".js",
-    };
+    options.outExtension = { ".js": ".js" };
   },
 });
